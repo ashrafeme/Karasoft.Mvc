4bec10949da1a4f72953d56459d20650369bb83a
ashrafeme <ashraf_eme@hotmail.com>
2015-03-25 09:44:11 +0300

    update basecontroller

--- BaseController.cs
+++ BaseController.cs
@@ -11,9 +11,25 @@ namespace Karasoft.Mvc
             AddAlert(AlertStyles.Success, message, dismissable);
         }
 
+        public void Success(IEnumerable<string> messages, bool dismissable = false)
+        {
+            foreach (var item in messages)
+            {
+                Success(string.Format("<li>{0}</li>", item), dismissable);
+            }
+        }
+
         public void Information(string message, bool dismissable = false)
         {
             AddAlert(AlertStyles.Information, message, dismissable);
+        }
+
+        public void Information(IEnumerable<string> messages, bool dismissable = false)
+        {
+            foreach (var item in messages)
+            {
+                Information(string.Format("<li>{0}</li>", item), dismissable);
+            }
         }
 
         public void Warning(string message, bool dismissable = false)
@@ -21,9 +37,25 @@ namespace Karasoft.Mvc
             AddAlert(AlertStyles.Warning, message, dismissable);
         }
 
+        public void Warning(IEnumerable<string> messages, bool dismissable = false)
+        {
+            foreach (var item in messages)
+            {
+                Warning(string.Format("<li>{0}</li>", item), dismissable);
+            }
+        }
+
         public void Danger(string message, bool dismissable = false)
         {
             AddAlert(AlertStyles.Danger, message, dismissable);
+        }
+
+        public void Danger(IEnumerable<string> messages, bool dismissable = false)
+        {
+            foreach (var item in messages)
+            {
+                Danger(string.Format("<li>{0}</li>", item), dismissable);
+            }
         }
 
         private void AddAlert(string alertStyle, string message, bool dismissable)
